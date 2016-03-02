e89d04ebde421d99b6be6ac23bda6e00e2c021f9
kjohnsson <kerstin.johnsson@hotmail.com>
2016-03-02 12:57:31 +0100

    critical bandwidth many modes

--- src/bandwidth_test.py
+++ src/bandwidth_test.py
@@ -23,7 +23,20 @@ def critical_bandwidth(data, I=(-np.inf, np.inf), htol=1e-3):
     return bisection_search_unimodal(0, hmax, htol, data, I)
 
 
+def critical_bandwidth_m_modes(data, m, I=(-np.inf, np.inf), htol=1e-3):
+        # I is interval over which density is tested for unimodality
+    hmax = (np.max(data)-np.min(data))/2.0
+    return bisection_search_most_m_modes(0, hmax, htol, data, m, I)
+
+
 def bisection_search_unimodal(hmin, hmax, htol, data, I):
+    '''
+        Assuming fun(xmax) < 0.
+    '''
+    return bisection_search_most_m_modes(hmin, hmax, htol, data, 1, I)
+
+
+def bisection_search_most_m_modes(hmin, hmax, htol, data, m, I):
     '''
         Assuming fun(xmax) < 0.
     '''
@@ -31,12 +44,16 @@ def bisection_search_unimodal(hmin, hmax, htol, data, I):
         return (hmin + hmax)/2.0
     hnew = (hmin + hmax)/2.0
     #print "hnew = {}".format(hnew)
-    if is_unimodal_kde(hnew, data, I):  # upper bound for bandwidth
-        return bisection_search_unimodal(hmin, hnew, htol, data, I)
-    return bisection_search_unimodal(hnew, hmax, htol, data, I)
+    if kde_has_at_most_m_modes(hnew, data, m, I):  # upper bound for bandwidth
+        return bisection_search_most_m_modes(hmin, hnew, htol, data, m, I)
+    return bisection_search_most_m_modes(hnew, hmax, htol, data, m, I)
 
 
 def is_unimodal_kde(h, data, I=(-np.inf, np.inf)):
+    return kde_has_at_most_m_modes(h, data, 1, I)
+
+
+def kde_has_at_most_m_modes(h, data, m, I=(-np.inf, np.inf)):
     # I is interval over which density is tested for unimodality
     xtol = h*0.1  # TODO: Compute error given xtol.
     kde = KDE(data, h)
@@ -51,7 +68,7 @@ def is_unimodal_kde(h, data, I=(-np.inf, np.inf)):
         # ax.plot(x, y)
         # ax.scatter(x, y, marker='+')
         # ax.scatter(x_new, y_new, marker='+', color='red')
-        if len(argrelextrema(np.hstack([[0], y, [0]]), np.greater)[0]) > 1:
+        if len(argrelextrema(np.hstack([[0], y, [0]]), np.greater)[0]) > m:
             return False
         if x[1] - x[0] < xtol:
             return True
@@ -90,9 +107,21 @@ if __name__ == '__main__':
         ax.plot(x_grid, KDE(data, h_crit).evaluate(x_grid), linewidth=2, color='black')
         plt.show()
 
-    if 1:
+    if 0:
         data = np.random.randn(1000)
         h = .5
         print "np.std(data) = {}".format(np.std(data))
         resamp = KernelDensity(kernel='gaussian', bandwidth=h).fit(data).sample(1000)/np.sqrt(1+h**2/np.var(data))
         print "np.std(resamp) = {}".format(np.std(resamp))
+
+    if 1:
+        N = 1000
+        data = np.hstack([np.random.randn(N/2), np.random.randn(N/4)+4])
+        h = 0.1
+        print "is_unimodal_kde(h, data) = {}".format(is_unimodal_kde(h, data))
+        #plt.show()
+        h_crit = critical_bandwidth_m_modes(data, 2)
+        x = np.linspace(-3, 8)
+        y = KernelDensity(kernel='gaussian', bandwidth=h_crit).fit(data.reshape(-1, 1)).score_samples(x.reshape(-1, 1))
+        plt.plot(x, np.exp(y))
+        plt.show()
