29ef4351cb0a58965c72621db9869d6cf75aaee1
Kerstin Johnsson <kerstin.johnsson@hotmail.com>
2016-05-24 12:54:11 +0200

    fp blurring

--- src/util/__init__.py
+++ src/util/__init__.py
@@ -1,4 +1,5 @@
 from MC_error_check import MC_error_check
 from printutil import print_all_ranks, print_rank0
+from frequency_polygon_blurring import fp_blurring
 
-__all__ = ['MC_error_check', 'print_all_ranks', 'print_rank0']
+__all__ = ['MC_error_check', 'print_all_ranks', 'print_rank0', 'fp_blurring']
